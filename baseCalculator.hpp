10fb23a56f15eaca05df86581fc626cf61173f79
Von <vonzedric012@gmail.com>
2025-04-26 23:12:37 +0800

    added initial logic for reading buffer syntax

--- baseCalculator.hpp
+++ baseCalculator.hpp
@@ -1,13 +1,46 @@
 #include <string>
 #include <vector>
 
-enum OP {ADD, SUB, MUL, DIV, EXP};
+//arranged according to order of operation precedence
+enum OP {SUB = 0, ADD = 1, DIV = 2, MUL = 3, EXP = 5};
 
-void readSyntax(std::string buffer) {
+bool isCharOp(char inp) {
+    return (
+        inp == '+' || inp == '-' || inp == 'x' ||
+        inp == '/' || inp == '^'
+        );
+}
+
+OP getOPCode(char inp) {
+    return SUB;
+}
+
+
+int readSyntax(std::string buffer) {
     const unsigned short int bufferSize = buffer.size();
+    bool isDecimalinNum = false;
+    std::string inpNum = "";
     std::vector<int> inputs;
+    std::vector<OP> ops;
 
     for(int i = 0; i < bufferSize; i++) {
+        if(inpNum == "" && isCharOp(buffer[i])) {
+            return 0;
+        }
 
+        if(isDecimalinNum && buffer[i] == '.') {
+            return 0;
+        } else {
+            inpNum += buffer[i];
+        }
+
+
+        if(i + 1 == bufferSize || isCharOp(buffer[i])) {
+            inputs.push_back(std::stoi(inpNum));
+            isDecimalinNum = false;
+            inpNum = "";
+        }
     }
+
+    return 1;
 }
